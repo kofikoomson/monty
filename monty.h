aa18bb5e7c080613e19e51eab1dcf440fa190253
kofikoomson <kofikoomson94@gmail.com>
2023-08-27 22:16:28 -0700

    monty.h fix

--- monty.h
+++ monty.h
@@ -39,3 +39,5 @@ void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *head);
+
+#endif
